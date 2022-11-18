4c9b62dbff227eddb07b45e2af4a343f095eae84
kiyohome <kiyohito.itoh@gmail.com>
2022-11-19 00:06:12 +0900

    StarterAssetsを素のNGOで動かした

---
+++ Samples~/MVS/App/StageConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Extreal.Integration.Multiplay.NGO.MVS.App
+{
+    [CreateAssetMenu(
+        menuName = "Config/" + nameof(StageConfig),
+        fileName = nameof(StageConfig))]
+    public class StageConfig : StageConfigBase<StageName, SceneName>
+    {
+    }
+}
---
+++ Samples~/MVS/Space/ClientNetworkAnimator.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Netcode.Components;
+
+namespace Extreal.Integration.Multiplay.NGO.MVS.Space
+{
+    public class ClientNetworkAnimator : NetworkAnimator
+    {
+        protected override bool OnIsServerAuthoritative() => false;
+    }
+}
