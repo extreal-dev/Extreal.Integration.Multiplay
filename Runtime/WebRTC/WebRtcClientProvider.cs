5f628ac3ac4376844fbc678db1010ef55a713456
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-08-02 15:21:42 +0900

    Add WebRTCTransport

---
+++ Runtime/WebRTC/WebRtcClientProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Extreal.Integration.P2P.WebRTC;
+
+namespace Extreal.Integration.Multiplay.NGO.WebRTC
+{
+    public static class WebRtcClientProvider
+    {
+        [SuppressMessage("Style", "CC0038"), SuppressMessage("Style", "IDE0022")]
+        public static WebRtcClient Provide(PeerClient peerClient)
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            return new NativeWebRtcClient(peerClient as NativePeerClient);
+#endif
+#if UNITY_WEBGL && !UNITY_EDITOR
+            return new WebGLWebRtcClient();
+#endif
+        }
+    }
+}
---
+++ Runtime/WebRTC/WebRtcTransportConnectionSetter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Unity.Netcode;
+
+namespace Extreal.Integration.Multiplay.NGO.WebRTC
+{
+    public class WebRtcTransportConnectionSetter : IConnectionSetter
+    {
+        private readonly WebRtcClient webRtcClient;
+
+        [SuppressMessage("Style", "CC0057")]
+        public WebRtcTransportConnectionSetter(WebRtcClient webRtcClient)
+            => this.webRtcClient = webRtcClient;
+
+        public Type TargetType => typeof(WebRtcTransport);
+
+        public void Set(NetworkTransport networkTransport, NgoConfig ngoConfig)
+        {
+            var webRtcTransport = networkTransport as WebRtcTransport;
+            webRtcTransport.SetWebRtcClient(webRtcClient);
+        }
+    }
+}
